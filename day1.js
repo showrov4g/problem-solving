150ca6e6bedec60bad166b6211a9823410b300f0
showrov4g <showrovg00@gmail.com>
2025-02-17 09:27:13 +0600

    secound problem solve

--- day1.js
+++ day1.js
@@ -4,3 +4,14 @@ function getArea(length, width){
 }
 
 // another function 
+
+function factorial(n) {
+    let fact = 1;
+    for (let i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+// উদাহরণ ব্যবহার:
+console.log(factorial(4)); // Output: 120
