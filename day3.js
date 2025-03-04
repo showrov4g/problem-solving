86eed9e8ab31cb7d7a3b106b495a16dd5c1e7ba1
showrov4g <showrovg00@gmail.com>
2025-03-04 09:10:10 +0600

    solve basic probielm

--- day3.js
+++ day3.js
@@ -1,9 +1,29 @@
-function addOrEvent(numbers){
-    if(numbers % 2 === 0){
-        return console.log("Even")
-    }else{
-        return console.log("Odd")
-    }
+function addOrEvent(numbers) {
+  if (numbers % 2 === 0) {
+    return console.log("Even");
+  } else {
+    return console.log("Odd");
+  }
 }
 
-addOrEvent(6)
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// average number calculations
+
+function getGrade(s1, s2, s3) {
+  // Code here
+  let score = (s1, s2, s3) / 2;
+  if (score >= 90) return 'A';
+  if (score >= 80) return 'B';
+  if (score >= 70) return 'C';
+  if (score >= 60) return 'D';
+  return 'F'; // 0 থেকে 59 হলে F
+}
+getGrade(90, 95, 90);
